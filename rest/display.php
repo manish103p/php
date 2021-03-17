e131f7704e2f32864a4921bbc1ea0820472fdd3f
manish103p <manish.parihar@somaiya.edu>
2021-03-17 22:28:24 +0530

    initial

---
+++ rest/display.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+    <html>
+    <body>
+        <center><h1>Rest API</h1></center>
+        <form action="fetch.php" method="POST">
+        <label for="details_id">Email:</label>
+        <input type="text" id="details_id" name="details_id"><br><br>
+        <input type="submit" value="Submit">
+        </form>
+    </body>
+</html>
